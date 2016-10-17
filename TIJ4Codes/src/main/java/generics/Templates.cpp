2fce9de82fdef4f9d396fc33bcd3004726404e72
phean <phean@qq.com>
2016-10-17 11:21:10 +0800

    15章泛型4

--- TIJ4Codes/src/main/java/generics/Templates.cpp
+++ TIJ4Codes/src/main/java/generics/Templates.cpp
@@ -6,7 +6,7 @@ template<class T> class Manipulator {
   T obj;
 public:
   Manipulator(T x) { obj = x; }
-  void manipulate() { obj.f(); }
+  void manipulate() { obj.f(); }//在没有边界的java泛型中，由于擦除，obj是不能知道f()的存在的，所以java中这句是报错的
 };
 
 class HasF {
